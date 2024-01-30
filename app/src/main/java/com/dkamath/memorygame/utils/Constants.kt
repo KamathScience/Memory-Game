d11f7dd0cf8b77fb810f0cbac44ac016b6f39318
Kamath <53545471+KamathScience@users.noreply.github.com>
2024-01-30 14:24:58 -0800

    Completed project

---
+++ app/src/main/java/com/dkamath/memorygame/utils/Constants.kt
@@ -0,0 +1,21 @@
+package com.dkamath.memorygame.utils
+
+import com.dkamath.memorygame.R
+
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_anchor,
+    R.drawable.ic_ball,
+    R.drawable.ic_bike,
+    R.drawable.ic_bunny,
+    R.drawable.ic_face,
+    R.drawable.ic_fire,
+    R.drawable.ic_flower,
+    R.drawable.ic_money,
+    R.drawable.ic_plane,
+    R.drawable.ic_sun,
+    R.drawable.ic_temple,
+    R.drawable.ic_water
+)
